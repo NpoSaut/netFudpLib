a50176da5171700b93d0f54351a7bf791dd3c640
Evgeniy Plyusnin <eninbox@gmail.com>
2013-11-18 12:25:54 +0600

    Убрана лишняя зависимость CanProg от WinUsbAppiDev

--- FudProtocol/CanProg.cs
+++ FudProtocol/CanProg.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using Communications.Appi;
 using Communications.Can;
 using Communications.Protocols.IsoTP;
 using Communications.Protocols.IsoTP.Exceptions;
