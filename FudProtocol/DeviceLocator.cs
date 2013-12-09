cf1dd752148ae0ada132de94e1cd34860f2e8f70
Evgeniy Plyusnin <eninbox@gmail.com>
2013-12-09 12:57:10 +0600

    Таймаут в DeviceLocator уменьшен до 100, т.к. я не успевал подхватить загрузчик монитора. И теперь таймер работает стабильнее

--- FudProtocol/DeviceLocator.cs
+++ FudProtocol/DeviceLocator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using Communications.Can;
@@ -24,8 +25,8 @@ namespace Fudp
         /// Находит в сети все устройства с заданным шаблоном билетов.
         /// </summary>
         /// <param name="Template">Шаблон билета устройства</param>
-        /// <param name="Timeout">Таймаут (в милисекундах). Таймаут отсчитываетс с момента получения последней IsoTP-транзакции, а не с момента начала опроса</param>
-        public List<DeviceTicket> LocateDevices(DeviceTicket Template, int Timeout = 500)
+        /// <param name="Timeout">Таймаут (в милисекундах). Таймаут отсчитывается с момента получения последней IsoTP-транзакции, а не с момента начала опроса</param>
+        public List<DeviceTicket> LocateDevices(DeviceTicket Template, int Timeout = 100)
         {
             return LocateDevices(Template, Port, Timeout);
         }
@@ -35,23 +36,25 @@ namespace Fudp
         /// </summary>
         /// <param name="Template">Шаблон билета устройства</param>
         /// <param name="OnPort">Can-порт, через который осуществляется работа</param>
-        /// <param name="Timeout">Таймаут (в милисекундах). Таймаут отсчитываетс с момента получения последней IsoTP-транзакции, а не с момента начала опроса</param>
+        /// <param name="Timeout">Таймаут (в милисекундах). Таймаут отсчитывается с момента получения последней IsoTP-транзакции, а не с момента начала опроса</param>
         /// <returns></returns>
-        public static List<DeviceTicket> LocateDevices(DeviceTicket Template, CanPort OnPort, int Timeout = 500)
+        public static List<DeviceTicket> LocateDevices(DeviceTicket Template, CanPort OnPort, int Timeout = 100)
         {
             using (var flow = new CanFlow(OnPort, CanProg.FuDev, CanProg.FuInit, CanProg.FuProg))
             {
-                if (Template.BlockSerialNumber != 0) Template.BlockSerialNumber = 0;
+                Template.BlockSerialNumber = 0;
 
-                var HelloMessage = new Messages.ProgInit(Template);
-                IsoTp.Send(flow, CanProg.FuInit, CanProg.FuDev, HelloMessage.Encode());
+                var helloMessage = new Messages.ProgInit(Template);
+                IsoTp.Send(flow, CanProg.FuInit, CanProg.FuDev, helloMessage.Encode());
 
                 var res = new List<DeviceTicket>();
-                while (true)
+                var sw = new Stopwatch();
+                sw.Start();
+                while (sw.ElapsedMilliseconds < Timeout)
                 {
                     try
                     {
-                        var tr = IsoTp.Receive(flow, CanProg.FuDev, CanProg.FuProg, TimeSpan.FromMilliseconds(Timeout));
+                        var tr = IsoTp.Receive(flow, CanProg.FuDev, CanProg.FuProg, TimeSpan.FromMilliseconds(Timeout - sw.ElapsedMilliseconds));
                         var msg = Messages.Message.DecodeMessage(tr.Data);
                         if (msg is Messages.ProgBCastResponse) res.Add((msg as Messages.ProgBCastResponse).Ticket);
                     }
