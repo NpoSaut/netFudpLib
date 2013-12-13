6701ce31b4be9d1183955eb9c76bcc0449b81d20
Evgeniy Plyusnin <eninbox@gmail.com>
2013-12-13 14:19:15 +0600

    Немного причёсан ProgList.cs

--- FudProtocol/Messages/ProgList.cs
+++ FudProtocol/Messages/ProgList.cs
@@ -13,29 +13,20 @@ namespace Fudp.Messages
         /// <summary>
         /// Список файлов
         /// </summary>
-        private List<DevFileInfo> listDevFileInfo;
-        public List<DevFileInfo> ListDevFileInfo
-        {
-            get { return listDevFileInfo; }
-            set { ;}
-        }
-        byte[] buff;
-        public byte[] Buff
-        {
-            get { return buff; }
-            set { ;}
-        }
+        public List<DevFileInfo> ListDevFileInfo { get; private set; }
+
+        public byte[] Buff { get; private set; }
 
         public override byte[] Encode()
         {            
             int disOffset = 1;
-            buff = new byte[4000];
-            buff[0] = MessageIdentifer;
-            for (int i = 0; i < listDevFileInfo.Count; i++)
+            Buff = new byte[4000];
+            Buff[0] = MessageIdentifer;
+            for (int i = 0; i < ListDevFileInfo.Count; i++)
             {
-                buff[i + disOffset] = (byte)listDevFileInfo[i].FileSize;
-                Buffer.BlockCopy(Encoding.GetEncoding(1251).GetBytes(listDevFileInfo[i].FileName), 0, buff, disOffset+1, listDevFileInfo[i].FileSize);
-                disOffset += listDevFileInfo[i].FileSize + 1;
+                Buff[i + disOffset] = (byte)ListDevFileInfo[i].FileSize;
+                Buffer.BlockCopy(Encoding.GetEncoding(1251).GetBytes(ListDevFileInfo[i].FileName), 0, Buff, disOffset+1, ListDevFileInfo[i].FileSize);
+                disOffset += ListDevFileInfo[i].FileSize + 1;
             }
             throw new NotImplementedException();
         }
@@ -45,26 +36,26 @@ namespace Fudp.Messages
         /// <param name="Data">Принятый массив байт</param>
         protected override void Decode(byte[] Data)
         {
-            listDevFileInfo = new List<DevFileInfo>();
-            byte[] bData = new byte[Data.Length - 1];
+            ListDevFileInfo = new List<DevFileInfo>();
+            var bData = new byte[Data.Length - 1];
             Buffer.BlockCopy(Data, 1, bData, 0, Data.Length - 1);
-            int FileNameSize = 0;
-            for (int i = 0; i < bData.Length; i += 9 + FileNameSize)
+            int fileNameSize = 0;
+            for (int i = 0; i < bData.Length; i += 9 + fileNameSize)
             {
-                FileNameSize = (int)bData[i];
-                byte[] bFileName = new byte[FileNameSize];
-                byte[] bFileSize = new byte[intSize];
-                byte[] bControlSum = new byte[intSize];
-                Buffer.BlockCopy(bData, 1 + i, bFileName, 0, FileNameSize);
-                Buffer.BlockCopy(bData, 1 + FileNameSize + i, bFileSize, 0, intSize);
-                Buffer.BlockCopy(bData, 5 + FileNameSize + i, bControlSum, 0, intSize);
+                fileNameSize = (int)bData[i];
+                var bFileName = new byte[fileNameSize];
+                var bFileSize = new byte[intSize];
+                var bControlSum = new byte[intSize];
+                Buffer.BlockCopy(bData, 1 + i, bFileName, 0, fileNameSize);
+                Buffer.BlockCopy(bData, 1 + fileNameSize + i, bFileSize, 0, intSize);
+                Buffer.BlockCopy(bData, 5 + fileNameSize + i, bControlSum, 0, intSize);
 
-                DevFileInfo FileInfo = new DevFileInfo(
+                var fileInfo = new DevFileInfo(
                     Name: Encoding.GetEncoding(1251).GetString(bFileName),
                     Size: BitConverter.ToInt32(bFileSize,0),
                     Checksum: BitConverter.ToUInt16(bControlSum,0));
                                 
-                listDevFileInfo.Add(FileInfo);
+                ListDevFileInfo.Add(fileInfo);
             }
         }
     }
