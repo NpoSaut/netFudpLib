625bec696e2561743bb62c2fc0384f151dca1e55
Evgeniy Plyusnin <eninbox@gmail.com>
2015-10-01 15:25:11 +0500

    Сделал FudpCrc16ChecksumProvider публичным

--- FudProtocol/FudpCrc16ChecksumProvider.cs
+++ FudProtocol/FudpCrc16ChecksumProvider.cs
@@ -1,6 +1,6 @@
 ﻿namespace Fudp
 {
-    internal class FudpCrc16ChecksumProvider : IChecksumProvider
+    public class FudpCrc16ChecksumProvider : IChecksumProvider
     {
         public ushort GetChecksum(byte[] Data) { return FudpCrc.CalcCrc(Data); }
     }
